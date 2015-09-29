b2cbd8686840dbb5d9d92f0a8b4189206700a686
usuario <usuario@10.22.143.15>
2015-09-29 10:26:20 +0100

    Second Commit
    Added CalculaHisto class

--- src/histogram/Histogram.java
+++ src/histogram/Histogram.java
@@ -5,50 +5,22 @@ import java.util.Map;
 
 public class Histogram <T> {
     
-    private HashMap<T, Integer> histogram = new HashMap<>();
+    private final Map<T, Integer> histogram;
     
-    public Histogram(T[] data) {
-        for (int i = 0; i < data.length; i++) {
-            Integer histogramValue = histogram.get(data[i]);
-            if(histogramValue == null) histogram.put(data[i], 1);
-            else histogram.put(data[i], ++histogramValue);
-        }
-    }
-
-    public int getHistogramOf(T object){
-        Integer histogramValue = histogram.get(object);
-        if(histogramValue == null) return 0;
-        return histogramValue;
+    public Integer increment(T key) {
+        return histogram.put(key, histogram.containsKey(key)? histogram.get(key) : 1);
     }
     
-    public class HistogramResult{
-        private T data;
-        private int value;
-
-        public HistogramResult(T data, int value) {
-            this.data = data;
-            this.value = value;
+    public Map<T, Integer> calculate(T[] vector){
+        for (T key : vector) {
+            Integer value = histogram.get(key);
+            if(value == null) histogram.put(key, 1);
+            else histogram.put(key, ++value);
         }
-
-        public T getData() {
-            return data;
-        }
-
-        public int getValue() {
-            return value;
-        }
-       
+        return histogram;
     }
-    /*
-    public HistogramResult[] getAllHistogram(){
-        HistogramResult result[] = new HistogramResult[histogram.size()];
-        int i = 0;
-        for (Map.Entry<T, Integer> key : histogram.entrySet()) {
-            result[i] = key.getKey();
-            result[i] = key.getValue();
-            i++;
-        }
-      
-        return result;
-    }*/
+    
+    public int getValueOf(T key) {
+        return histogram.get(key);
+    }
 }
