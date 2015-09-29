745d504d769af8e454b2789d331419dd2978886d
usuario <usuario@10.22.143.15>
2015-09-29 10:12:27 +0100

    First Commit
    Added histogram class

--- src/histogram/HistogramProgram.java
+++ src/histogram/HistogramProgram.java
@@ -1,13 +1,17 @@
 package histogram;
 
+import java.util.HashMap;
+
 public class HistogramProgram {
     public static void main(String[] args) {
         //int vector[] = {2,2,11,4,2,6,11,2,3,5,11,5};
         String vector[] = {"Ana", "Juan", "Pedro", "Lucia",
             "Ana", "Juan", "Pedro"};
-        Histogram histogram = new Histogram(vector);
-        System.out.println(histogram.getHistogramOf(1));
-        System.out.println(histogram.getHistogramOf(2));
-        
+        Histogram<String> histogram = new Histogram(vector);
+        HashMap histogramStats = histogram.calculate();
+        /*for (Map.Entry<String><Integer> result : histogramStats.entrySet()) {
+            System.out.println(result.getKey() + " " +result.getValue());     
+        }*/
+
     }
 }
